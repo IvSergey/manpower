c4e5d3bd1af5fa856658441cf6c6622e12ca1473
IvSergey <imas.spb1988@gmail.com>
2020-09-11 19:26:02 +0300

    Finally

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -56,6 +56,7 @@ form.addEventListener('submit', function (e) {
   let errorName = document.querySelector('#errorName');
   let errorEmail = document.querySelector('#errorEmail');
   let inValidChecked = document.querySelector('#inValidChecked');
+  let inValidSelected = document.querySelector('#inValidSelected');
 
 
 
@@ -63,45 +64,66 @@ form.addEventListener('submit', function (e) {
   let reg2 = /^([A-Za-z0-9_\-\])+@([A-Za-z0-9_\-\])+([A-Za-z]{2,4})$/;
 
   let errors = 0;
+  let failedName = 0;
+  let failedEmail = 0;
+  let failedCountry = 0;
+  let failedCheckbox = 0;
 
-  if (name != '' && email != '' && country !== 'Select your country') {
-    if (checkbox.checked) {
-      if (reg1.test(name)) {
-        // alert('Поле "Enter your name" может содержать только русские и английские символы, пробелы и дефисы');
-        errorName.classList.add('active');
-        setTimeout(() => errorName.classList.remove('active'), 3000);
-        errors = 1;
-
-      }
-      else {
-        if (reg2.test(email) == false) {
-          // alert('Неверный формат почты');
-          errorEmail.classList.add('active');
-          setTimeout(() => errorEmail.classList.remove('active'), 3000);
-          errors = 1;
-        } else {
-          //alert('OK');
-          errors = 0;
-        }
-        //alert('OK')
-
-      }
-    } else {
-      // alert('Поставьте галочку, чтобы подтвердить, что вы согласны с условиями');
-      inValidChecked.classList.add('active');
-      setTimeout(() => errorEmail.classList.remove('active'), 3000);
-      errors = 1;
-    }
-
-
-  } else {
-    alert('Fill all fields');
+  if (country == 'Select your country') {
+    errors = 1;
+    failedCountry = 1;
+  }
+  if (!checkbox.checked) {
+    failedCheckbox = 1;
     errors = 1;
   }
 
+  if (name == '' || reg1.test(name)) {
+    //alert('Поле "Enter your name" может содержать только русские и английские символы, пробелы и дефисы');
+    //errorName.classList.add('active');
+    //setTimeout(() => errorName.classList.remove('active'), 3000);
+    errors = 1;
+    failedName = 1;
+  }
+
+  if (email == '' || !reg2.test(email)) {
+    //alert('Неверный формат почты');
+    errors = 1;
+    //errorEmail.classList.add('active');
+    //setTimeout(() => errorEmail.classList.remove('active'), 3000);
+    failedEmail = 1;
+  }
+  //alert('OK')
+
+  if (failedName > 0) {
+    errorName.classList.add('active');
+    setTimeout(() => errorName.classList.remove('active'), 3000);
+  }
+  if (failedEmail > 0) {
+    errorEmail.classList.add('active');
+    setTimeout(() => errorEmail.classList.remove('active'), 3000);
+  }
+  if (failedCountry > 0) {
+    // alert('');
+    inValidSelected.classList.add('active');
+    setTimeout(() => inValidSelected.classList.remove('active'), 3000);
+  }
+  if (failedCheckbox > 0) {
+    inValidChecked.classList.add('active');
+    setTimeout(() => inValidChecked.classList.remove('active'), 3000);
+  }
+
+
+
+
+
   if (errors > 0) {
     e.preventDefault();
+  } else {
+    alert('Всё отправилось');
   }
+
+
 
 });
 
@@ -111,13 +133,31 @@ let modal = document.querySelector('#modal');
 let modalOverlay = document.querySelector('#modal-overlay');
 let closeButton = document.querySelector('#close-button');
 // let openButton = document.querySelector('#open-button');
+const body = document.body;
+const btnAccept = document.querySelector('#btn__accept');
+const btnCancel = document.querySelector('#btn__cancel');
 
 
 closeButton.addEventListener('click', function () {
+  body.style.overflow = 'auto';
   modal.classList.toggle('closed');
   modalOverlay.classList.toggle('closed');
-  wrapper.classList.toggle('active');
+
 });
+btnAccept.addEventListener('click', function () {
+  body.style.overflow = 'auto';
+  modal.classList.toggle('closed');
+  modalOverlay.classList.toggle('closed');
+
+});
+btnCancel.addEventListener('click', function () {
+  body.style.overflow = 'auto';
+  modal.classList.toggle('closed');
+  modalOverlay.classList.toggle('closed');
+
+});
+
+
 
 // openButton.addEventListener('click', function() {
 // 	  modal.classList.toggle('closed');
@@ -128,8 +168,12 @@ closeButton.addEventListener('click', function () {
 let elems = document.querySelectorAll('.description__item');
 for (let i = 0; i <= elems.length; i++) {
   elems[i].addEventListener('click', function () {
+
+    body.style.overflow = 'hidden';
+    body.style.backgroundСolor = 'rgba(0,0,0,.5)';
+    body.style.fiter = 'brightness(.5)';
     modal.classList.toggle('closed');
     modalOverlay.classList.toggle('closed');
-    
+
   });
 }
